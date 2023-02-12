de987bd22e50fbb8d77358312e79387234a674b2
Lenovo <Lenovo@LAPTOP-G7UMRBT2>
2023-02-12 15:57:10 +0530

    first commit

---
+++ dayseven/src/org/tnsif/abstractiondemo/ButterChicken.java
@@ -0,0 +1,17 @@
+package org.tnsif.abstractiondemo;
+//implemented class
+public class ButterChicken extends Menu{
+	
+	/* implement all the abstract method of abstract class */
+	@Override
+	void recipe() {
+		System.out.println("Wash and cook a chicken");
+	}
+
+	@Override
+	void foodType() {
+		System.out.println("Non-Veg");
+		
+	}
+
+}
---
+++ dayseven/src/org/tnsif/polymorphismdemo/SBI.java
@@ -0,0 +1,12 @@
+package org.tnsif.polymorphismdemo;
+//child class
+public class SBI extends RBI {
+
+	float getInterest ()
+	{
+		System.out.println(s.getInterest());
+		return 12.8f;
+	}
+	
+	
+}
