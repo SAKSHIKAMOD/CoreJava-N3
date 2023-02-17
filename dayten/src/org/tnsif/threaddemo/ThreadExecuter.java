2e0c821c17a8e34d4413d262b0d2f8d786d374f4
Lenovo <Lenovo@LAPTOP-G7UMRBT2>
2023-02-17 09:16:31 +0530

    first commit

---
+++ dayten/src/org/tnsif/threaddemo/ThreadExecuter.java
@@ -0,0 +1,21 @@
+package org.tnsif.threaddemo;
+//program to demonstrate on thread class
+//driver class
+public class ThreadExecuter {
+
+	public static void main(String[] args) {
+		SimpleThreadProgram p=new SimpleThreadProgram();
+		//used to change thread name 
+		p.setName("Java Method");
+		//used to change the priority
+		p.setPriority(8);
+		//when thread calls to start method, it will come to run method 
+		p.start();
+		p.run();
+		//Ones we call start() method, again we can not restart
+        //p.start();
+		System.out.println("Current Thread "+p);
+		
+	}
+
+}
---
+++ dayten/src/org/tnsif/usingrunnable/ThreadLifeCycleDemo.java
@@ -0,0 +1,18 @@
+package org.tnsif.usingrunnable;
+
+import org.tnsif.threadlifecycle.SoftSkill;
+import org.tnsif.threadlifecycle.Tech;
+
+public class ThreadLifeCycleDemo {
+
+	public static void main(String[] args) {
+		Tech t=new Tech();
+		SoftSkill s=new SoftSkill();
+		t.start();
+		//Move control to another thread
+		t.yield();
+		
+		s.start();
+	}
+
+}
